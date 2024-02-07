b1d467a1d0e45a8bbb275b6acfe8ce32186bc084
Santiago Coria <santiago_coria@hotmail.com>
2024-02-07 20:22:32 -0300

    Firt project

---
+++ src/Playlist/Playlist.java
@@ -0,0 +1,26 @@
+package Playlist;
+
+public class Playlist {
+    private String name;
+    private int currentTrack;
+
+    public void add(Track track){
+
+    }
+    public void add(Playlist other){
+
+    }
+    public boolean remove (Track track){
+        return false;
+
+    }
+    public Track getCurrentTrack(){
+        return null;
+    }
+    public boolean contains(String title){
+        return false;
+    }
+    public void previous(){
+
+    }
+}
---
+++ src/Playlist/Track.java
@@ -0,0 +1,20 @@
+package Playlist;
+
+public class Track {
+    private String title;
+    private String artist;
+
+    public void play(){
+
+    }
+
+    public void pause(){
+
+    }
+    public void stop(){
+
+    }
+    public boolean isPlaying(){
+        return false;
+    }
+}
